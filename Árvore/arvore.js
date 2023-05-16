9b30b06cd3d137678938fe1b2c8fd615a258b452
Bianca Ramos <124745167+biaazinha@users.noreply.github.com>
2023-05-16 10:24:35 -0300

    arvore

---
+++ Árvore/arvore.js
@@ -0,0 +1,41 @@
+/*
+
+Uma árvore é uma estrutura de dados não linear frequentemente utilizada para rápido acesso aos dados.
+Nota-se que a recursão é utilizada como ferramenta de definição.
+Um árvore é uma estrutura sofisticada cuja definição por meio de recursão é elegante e eficaz.
+Uma árvore, com tipo T, pode ser definida recursivamente da seguinte forma:
+◦Uma árvore (estrutura) vazia ou
+◦Um nó do tipo T associado a um número finito de estruturas disjuntas de árvore do mesmo tipo T, denominadas subárvores
+
+
+Cada elemento de uma árvore é chamado nó da árvore.
+
+Normalmente, as árvores são desenhadas com a raiz no topo.
+
+No exemplo, o nó A possui três subárvores cujas raízes são B, C e D.
+
+Um nó sem filhos é chamado folha (ou terminal).
+
+Um nó com filhos é denominado não-folha, não-terminal ou interior.
+
+O grau de um nó é determinado pelo número de filhos (imediatos) deste nó.
+
+O nível de um nó é definido como:
+◦em geral, o nó raiz está no nível 0 (zero);
+◦os outros nós possuem um nível a mais que o nível de seu nó pai.
+
+A profundidade ou altura de uma árvore significa o nível máximo de qualquer folha na árvore, ou seja, o tamanho do percurso mais distante da raiz até qualquer folha.
+
+
+Uma árvore binária é dita balanceada se, para cada nó, as alturas de suas duas subárvores diferem de, no máximo, 1.
+Uma árvore binária é perfeitamente balanceada se, para cada nó, os números de nós das suas duas subárvores diferem de, no máximo, 1.
+
+Árvores binárias são árvores ordenadas de grau 2.
+Uma árvore estritamente binária é aquela que tem nós que possuem 0 ou dois filhos.
+Uma árvore binária completa é aquela que é estritamente binária de nível d e com todos os nós-folhas no mesmo nível d.
+
+Pré ordem: da ponta, da esquerda para direita 
+In ordem: da ponta esquerda de baixo para direita em cima
+Pós ordem: de baixo para cima da esquerda 
+
+*/
