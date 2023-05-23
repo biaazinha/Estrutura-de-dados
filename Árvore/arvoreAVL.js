becde1251a1e86a14a1b078f423c4581cb2fc215
Bianca Ramos <124745167+biaazinha@users.noreply.github.com>
2023-05-23 08:26:52 -0300

    Update arvoreAVL.js

--- Árvore/arvoreAVL.js
+++ Árvore/arvoreAVL.js
@@ -3,4 +3,9 @@
 Se fator de balanceamento é positivo, as rotações são feitas à direita.
 Se fator de balanceamento é negativo, as rotações são feitas à esquerda.
 
+Quando o erro do balanceamento dá com sinais diferentes a rotação é dupla
+Quando o erro do balanceamento dá com sinais igauis a rotação é simples
+
+Quando tiramos um elemento no meio da arvore, substituimos pelo maior da esquerda
+
 */
