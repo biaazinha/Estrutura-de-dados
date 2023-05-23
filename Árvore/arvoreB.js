9a47ac7eef23e5806efb682f30058ab2dbd7aa87
Bianca Ramos <124745167+biaazinha@users.noreply.github.com>
2023-05-23 09:07:07 -0300

    Create arvoreB.js

---
+++ Árvore/arvoreB.js
@@ -0,0 +1,20 @@
+/*
+
+-balanceadas
+-armazenamento secundario
+-cada nó tem varios elementos
+
+a propria inserção e remoção deixa a arvore balanceada
+
+só a raiz pode ficar menor que o numero minimo
+apesar disso, o numero de filhos da raiz não pode ultrapassar mais que um da raiz
+
+//        correto          errado
+//           1                1
+//        2     2       3     2     1
+
+
+QUANDO ADICIONA um elemento - o do MEIO sobe e ela se divide ao meio (subo)
+QUANDO REMOVE um elemento - duas folhas se juntam (diminuo)
+
+*/
